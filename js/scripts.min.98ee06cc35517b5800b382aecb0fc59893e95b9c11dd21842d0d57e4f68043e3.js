52a10f678cac76f40cb90ec1fb19ec0a170fb80f
JeremyRubin <JeremyRubin@users.noreply.github.com>
2022-03-23 15:44:45 +0000

    deploy: dbe3eacd92f092846f545c4982b704e56381b4a3

--- js/scripts.min.98ee06cc35517b5800b382aecb0fc59893e95b9c11dd21842d0d57e4f68043e3.js
+++
@@ -1,1 +0,0 @@
-var body=document.querySelector('body'),menuTrigger=document.querySelector('#toggle-main-menu-mobile'),menuContainer=document.querySelector('#main-menu-mobile');menuTrigger.onclick=function(){menuContainer.classList.toggle('open'),menuTrigger.classList.toggle('is-active'),body.classList.toggle('lock-scroll')}
